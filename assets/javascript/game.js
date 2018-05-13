bd8bd2ff6d3e54374a0ac06e4c2d7ba4e53add25
unknown <max.yeamans@gmail.com>
2018-05-13 10:18:21 -0700

    Added some code to the game js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,15 @@
 var userInfo = {
     "Wins" : 0,
     "Losses" : 0,
-    "Ties" : 0
+    "GuessesLeft" : 0,
+    "GameWon" : false,
+    "GameWord" : null,
 }
 
-var gameWords = ["hangman","programming"];
+var gameWords = ["hangman","programming","parameter","argument","stylesheet"];
+
+// Picks a word from the word array at random
+function PickWord(words) {
+    this.GameWord = gameWords[ Math.floor( Math.random() * words.length) ];
+}
 
