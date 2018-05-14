6f74ffecedaa6726c918241a29f85d8b9ec18e9e
Max Yeamans <max.yeamans@gmail.com>
2018-05-14 06:01:29 -0700

    Added better win logic, but there are sometimes issues when the word is parameter (win condition not recognized) or programming (iterator goes up twice for each letter if the letter appears twice in the word).

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,27 +1,32 @@
 var GameData = {
     "Wins": 0,
     "Losses": 0,
-    "GuessesLeft": 10,
+    "GuessesLeft": 3,
     "GameWon": false,
     "GameWord": null,
-    "LettersRemaining": null,
+    "CorrectLetters": 0,
     "GuessedLetters": [],
     "WordBuilder": ["_"],
     "WordSoFar": "",
 
     // Assign the game's word from an array of potential words
-    PickWord: function(words) {
-        this.GameWord = gameWords[Math.floor(Math.random() * words.length)];
+    PickWord: function () {
+        // Reset game stats
+        this.ResetStats();
+        // Show stats
+        console.log("Wins:", this.Wins);
+        console.log("Losses:", this.Losses);
+        // Pick a new word        
+        this.GameWord = gameWords[Math.floor(Math.random() * gameWords.length)];
         console.log("This game's word:", this.GameWord);
-        this.LettersRemaining = this.GameWord.length;
-        console.log("Letters remaining in the word:", this.LettersRemaining);
+        console.log("Game has been won:", this.GameWon);
         // Create a string with underscores equal to length of game's word
-        for( i = 0; i < this.GameWord.length - 1; i++) {
-            this.WordBuilder.push(" _");
+        for (i = 0; i < this.GameWord.length - 1; i++) {
+            this.WordBuilder.push("_");
         };
     },
 
-    LetterPicked: function(letter) {
+    LetterPicked: function (letter) {
         // Set the letter to lowercase justincase, might not be needed
         // letter = letter.toLowerCase();
         // Check to see if the letter has already been guessed
@@ -32,6 +37,7 @@ var GameData = {
         // Do this if the letter isn't in the word
         else if (this.GameWord.indexOf(letter) < 0) {
             console.log("That letter isn't in the word!");
+            // Push the wrong letter to the GuessedLetters array
             this.GuessedLetters.push(letter);
             console.log("Guessed letters:", this.GuessedLetters);
             // Decrement guesses left
@@ -40,14 +46,59 @@ var GameData = {
         }
         // Do this if the letter is in the word
         else {
+            // Add the letter to the correct letters array
             for (j = 0; j < this.GameWord.length; j++) {
                 if (this.GameWord.charAt(j) === letter) {
                     this.WordBuilder[j] = letter;
+                    this.CorrectLetters++;
                 }
-                
             }
-            console.log("The word so far:", this.WordBuilder);
+            // Create a string from the correct letters array.
+            // Probably won't need this once I'm displaying it on a web page.
+            this.WordSoFar = "";    // Have to erase the string before recreating it.
+            for (k = 0; k < this.GameWord.length; k++) {
+                this.WordSoFar += this.WordBuilder[k];
+            }
+            console.log("The word so far:", this.WordSoFar);
         }
+
+        console.log("Correctly guessed letters:", this.CorrectLetters);
+        this.CheckForWin();
+    },
+
+    CheckForWin: function () {
+
+        // If the number of correctly guessed letters
+        // equals the length of the word, user wins.
+        if (this.CorrectLetters == this.GameWord.length) {
+            console.log("Congrats! You win!");
+            console.log("");
+            this.Wins++;
+            this.PickWord();
+            return;
+        }
+
+        else if (this.GuessesLeft == 0) {
+            console.log("You lose! Good day, sir!");
+            console.log("");
+            this.Losses++;
+            this.PickWord();
+            return;
+        }
+
+        // else {
+        //     return;
+        // }
+    },
+
+    // Reset game stats
+    ResetStats: function () {
+        this.GuessesLeft = 3;
+        this.GameWon = false;
+        this.CorrectLetters = 0;
+        this.GuessedLetters = [];
+        this.WordBuilder = ["_"];
+        this.WordSoFar = "";
     }
 
 }
@@ -56,9 +107,9 @@ var GameData = {
 var gameWords = ["hangman", "programming", "parameter", "argument", "stylesheet"];
 
 // Choose the game's word
-GameData.PickWord(gameWords);
+GameData.PickWord();
 
-document.onkeyup = function(event) {
+document.onkeyup = function (event) {
 
     var userInput = event.key.toLowerCase();
     console.log("User input:", userInput);
